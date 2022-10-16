4469767d4ca29504e91db57467531cb3ae4baf26
outwrest <0utWrest@protonmail.com>
2022-10-16 10:48:06 -0500

    updated  picture capture to talk to backend

--- src/views/pictureCapture.vue
+++ src/views/pictureCapture.vue
@@ -51,6 +51,7 @@ import {
 } from '@ionic/vue';
 import axios from 'axios';
 import FormData from 'form-data';
+import Noty from 'noty';
 
 export default defineComponent({
   name: 'pictureCapture',
@@ -74,30 +75,47 @@ export default defineComponent({
       await this.takePhoto();
 
       // start loader
-
-      // const formData = new FormData();
-      // if (this.photos.length > 0 && this.photos[0].webviewPath) {
-      //   const imagebuffer = await fetch(this.photos[0].webviewPath);
-
-      //   formData.append('file', await imagebuffer.blob(), 'image.jpg');
-      //   axios.post(`${process.env.RECYCLE_API}/search`, formData, {
-      //     headers: {
-      //       'Content-Type': 'multipart/form-data',
-      //     },
-      //   }).then((response) => {
-      //     console.log(response);
-      //     this.$router.push('/tabs/recyclingMap');
-      //     // stop loader
-      //   }).catch((error) => {
-      //     console.log(error);
-      //     // stop loader, error page
-      //   });
-      // }
-      this.$router.push('/tabs/recyclingMap');
+      const formData = new FormData();
+      if (this.photos.length > 0 && this.photos[0].webviewPath) {
+        const imagebuffer = await fetch(this.photos[0].webviewPath);
+        console.log(process.env.VUE_APP_RECYCLE_API)
+        formData.append('uploaded_img', await imagebuffer.blob(), 'image.jpg');
+        axios.post(`${process.env.VUE_APP_RECYCLE_API}/search`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }).then((response) => {
+          console.log(response);
+          
+          let conf = response.data[0]['sim'];
+          if (conf < 0.5) {
+            // this.$router.push('/tabs/recyclingMap');
+            const material_id = response.data[0]['id'];
+            
+            // TODO
+          }
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+      
+      // stop loader
+      this.$router.push({ path: '/tabs/pictureCapture', query: { error: "Prediction not confident, please try to take an image in different lightning." }});
       // go back and error
     }
   },
   setup() {
+    let uri = window.location.search.substring(1); 
+    let params = new URLSearchParams(uri);
+    let error = params.get('error');
+    if (error) {
+      console.log(error);
+      new Noty({
+        type: 'error',
+        text: error,
+      }).show();
+    }
+
     const { photos, deletePhoto, takePhoto } = usePhotoGallery();
     return {
       photos,
